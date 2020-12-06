5b08cd95aef8cb631b0f1342c37873f447bdd192
mlange-42 <martin_lange_@gmx.net>
2020-12-06 15:00:11 +0100

    walk through repo in current path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+use git2::{Commit, Error, Repository, Time};
+
+struct Args {}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args {};
+    match run(&args) {
+        Ok(()) => {}
+        Err(e) => println!("error: {}", e),
+    }
+}
+
+fn run(_args: &Args) -> Result<(), Error> {
+    let path = ".";
+    let repo = Repository::open(path)?;
+    let mut revwalk = repo.revwalk()?;
+
+    revwalk.set_sorting(git2::Sort::TOPOLOGICAL)?;
+
+    revwalk.push_head()?;
+
+    for oid in revwalk {
+        let oid = oid?;
+        let commit = repo.find_commit(oid).unwrap();
+        print_commit(&commit);
+    }
+    Ok(())
+}
+
+fn print_commit(commit: &Commit) {
+    println!("commit {}", commit.id());
+
+    if commit.parents().len() > 1 {
+        print!("Merge:");
+        for id in commit.parent_ids() {
+            print!(" {:.8}", id);
+        }
+        println!();
+    }
+
+    let author = commit.author();
+    println!("Author: {}", author);
+    print_time(&author.when(), "Date:   ");
+    println!();
+
+    for line in String::from_utf8_lossy(commit.message_bytes()).lines() {
+        println!("    {}", line);
+    }
+    println!();
+}
+
+fn print_time(time: &Time, prefix: &str) {
+    let (offset, sign) = match time.offset_minutes() {
+        n if n < 0 => (-n, '-'),
+        n => (n, '+'),
+    };
+    let (hours, minutes) = (offset / 60, offset % 60);
+    let ts = time::Timespec::new(time.seconds() + (time.offset_minutes() as i64) * 60, 0);
+    let time = time::at(ts);
+
+    println!(
+        "{}{} {}{:02}{:02}",
+        prefix,
+        time.strftime("%a %b %e %T %Y").unwrap(),
+        sign,
+        hours,
+        minutes
+    );
 }
