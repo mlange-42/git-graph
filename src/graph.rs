21459da37f82b287dc4e4b26e9fa17ba5f9ad250
mlange-42 <martin_lange_@gmx.net>
2020-12-07 13:17:00 +0100

    assign branches to commits

--- src/graph.rs
+++ src/graph.rs
@@ -1,30 +1,55 @@
-use git2::{Commit, Error, Oid, Repository};
+use git2::{BranchType, Commit, Error, Oid, Repository};
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct GitGraph {
     pub repository: Repository,
     pub commits: Vec<CommitInfo>,
+    pub indices: HashMap<Oid, usize>,
 }
 
 impl GitGraph {
     pub fn new(path: &str) -> Result<Self, Error> {
         let repository = Repository::open(path)?;
-        let mut revwalk = repository.revwalk()?;
+        let mut walk = repository.revwalk()?;
 
-        revwalk.set_sorting(git2::Sort::TOPOLOGICAL)?;
-        revwalk.push_head()?;
+        walk.set_sorting(git2::Sort::TOPOLOGICAL)?;
+        walk.push_head()?;
 
         let mut commits = Vec::new();
-        for oid in revwalk {
+        let mut indices = HashMap::new();
+        for (idx, oid) in walk.enumerate() {
             let oid = oid?;
             let commit = repository.find_commit(oid).unwrap();
             commits.push(CommitInfo::new(&commit));
+            indices.insert(oid, idx);
         }
 
-        Ok(GitGraph {
+        let mut graph = GitGraph {
             repository,
             commits,
-        })
+            indices,
+        };
+        graph.assign_branches()?;
+
+        Ok(graph)
+    }
+    fn assign_branches(&mut self) -> Result<(), Error> {
+        let branches = self.repository.branches(None)?;
+        for branch in branches {
+            let (branch, branch_type) = branch?;
+            if branch_type == BranchType::Local {
+                let reference = branch.get();
+                if let Some(oid) = reference.target() {
+                    let idx = self.indices[&oid];
+                    if let Some(name) = reference.name() {
+                        self.commits[idx].branches.push(name[11..].to_string());
+                    }
+                }
+            }
+        }
+
+        Ok(())
     }
     pub fn commit(&self, id: Oid) -> Result<Commit, Error> {
         self.repository.find_commit(id)
@@ -34,10 +59,16 @@ impl GitGraph {
 #[allow(dead_code)]
 pub struct CommitInfo {
     pub oid: Oid,
+    pub branches: Vec<String>,
+    pub branch_traces: Vec<String>,
 }
 
 impl CommitInfo {
     fn new(commit: &Commit) -> Self {
-        CommitInfo { oid: commit.id() }
+        CommitInfo {
+            oid: commit.id(),
+            branches: Vec::new(),
+            branch_traces: Vec::new(),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,30 @@ fn run(_args: &Args) -> Result<(), Error> {
     let path = ".";
     let graph = GitGraph::new(path)?;
     for info in &graph.commits {
-        print_commit_short(&graph.commit(info.oid)?);
+        let commit = &graph.commit(info.oid)?;
+        print_commit_short(commit, &info.branches);
     }
     Ok(())
 }
 
 #[allow(dead_code)]
-fn print_commit_short(commit: &Commit) {
+fn print_commit_short(commit: &Commit, branches: &Vec<String>) {
     let symbol = if commit.parents().len() > 1 {
         "\u{25CB}"
     } else {
         "\u{25CF}"
     };
+    let branch_str = if branches.is_empty() {
+        "".to_string()
+    } else {
+        format!(" ({})", branches.join(", "))
+    };
+
     let str = format!(
-        "{} {} {}",
+        "{} {}{} {}",
         symbol,
         &commit.id().to_string()[0..7],
+        branch_str,
         &commit.summary().unwrap_or("---")
     );
     println!("{}", str);
