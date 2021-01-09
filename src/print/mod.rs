ea9e872bebbe4bbcd62365f7eb805e0c1bffac88
Martin Lange <martin_lange_@gmx.net>
2021-01-09 21:20:38 +0100

    renamed branches to all_branches, store actual branches and tags as indices into all_branches

--- src/print/mod.rs
+++ src/print/mod.rs
@@ -16,14 +16,14 @@ fn get_deviate_index(graph: &GitGraph, index: usize, par_index: usize) -> usize 
     let info = &graph.commits[index];
 
     let par_info = &graph.commits[par_index];
-    let par_branch = &graph.branches[par_info.branch_trace.unwrap()];
+    let par_branch = &graph.all_branches[par_info.branch_trace.unwrap()];
 
     let mut min_split_idx = index;
     for sibling_oid in &par_info.children {
         if let Some(&sibling_index) = graph.indices.get(sibling_oid) {
             if let Some(sibling) = graph.commits.get(sibling_index) {
                 if let Some(sibling_trace) = sibling.branch_trace {
-                    let sibling_branch = &graph.branches[sibling_trace];
+                    let sibling_branch = &graph.all_branches[sibling_trace];
                     if sibling_oid != &info.oid
                         && sibling_branch.visual.column == par_branch.visual.column
                         && sibling_index > min_split_idx
