55d64756467c0d166c9f8ad1bcf33df4650c351e
Karl Heitmann <khf1988@gmail.com>
2023-06-15 23:58:23 -0400

    Uses git-graph.toml to parse commits message format

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,11 @@ use std::time::Instant;
 
 const REPO_CONFIG_FILE: &str = "git-graph.toml";
 
+#[derive(serde_derive::Serialize, serde_derive::Deserialize)]
+pub struct CommitFormatToml {
+    pub format: String,
+}
+
 fn main() {
     std::process::exit(match from_args() {
         Ok(_) => 0,
@@ -289,7 +294,19 @@ fn from_args() -> Result<(), String> {
     )?;
 
     let format = match matches.get_one::<String>("format") {
-        None => CommitFormat::OneLine,
+        None => { 
+            let mut config_path = std::path::PathBuf::from(repository.path());
+            config_path.push(REPO_CONFIG_FILE);
+            if config_path.exists() {
+                let commit_format_toml: CommitFormatToml = toml::from_str(
+                    &std::fs::read_to_string(config_path).map_err(|err| err.to_string())?,
+                )
+                .map_err(|err| err.to_string()).unwrap();
+                CommitFormat::from_str(&commit_format_toml.format)?
+            } else {
+                CommitFormat::OneLine
+            }
+        },
         Some(str) => CommitFormat::from_str(str)?,
     };
 
